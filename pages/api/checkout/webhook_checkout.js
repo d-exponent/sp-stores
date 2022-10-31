6740e30fcc35ce26931b0b0a5b8374d8a9dc5590
Desmond Odion <d_clipse@icloud.com>
2022-10-31 17:00:45 +0000

    Still tracing webhook_checkout bug

--- pages/api/checkout/webhook_checkout.js
+++ pages/api/checkout/webhook_checkout.js
@@ -5,87 +5,71 @@ import Order from '../../../models/order-model'
 import User from '../../../models/user-model'
 
 const handler = async (req, res) => {
-	if (req.method !== 'POST' || !req.body) {
+	if (req.method !== 'POST') {
+		res.status(404).send('Only post request allowed')
 		return
 	}
 
-	const EVENT = req.body
-	const AUTO_USER_PASSWORD = process.env.ON_PAY_PAYSTACK_WEBHOOK_USER
-	let IS_USER = false
-
 	const hash = crypto
 		.createHmac('sha512', process.env.PAYSTACK_SECRET_KEY)
-		.update(JSON.stringify(EVENT))
+		.update(JSON.stringify(req.body))
 		.digest('hex')
 
 	// Validate request payload from paystack
 	if (hash == req.headers['x-paystack-signature']) {
-		res.status(200).send(200)
-		const formatedDateNow = new Date(Date.now()).toUTCString()
-		console.log('👍 Received Paystack EVENT at: =>' + formatedDateNow)
+		res.send(200)
 
-		console.log('🧰Event received', EVENT)
-		console.log('🧰EVENT DATA', EVENT.data)
-		console.log('🧰EVENT METADATA', EVENT.metadata)
-		const clientEmail = EVENT.customer.email
-		const clientMetadata = EVENT.data.metadata['customer_details']
+		const EVENT = req.body
+		let IS_USER = false
+		console.log('👍 Received Paystack EVENT at: =>' + new Date(Date.now()).toISOString())
+
+		const eventData = EVENT.data
+		const eventMetadata = eventData.metadata
+		const bagitems = eventMetadata['bag_items']
+		const { email, firstName, lastName } = eventMetadata['customer_details']
+
+		console.log('🧰🧰 EVENT DATA', eventData)
+		console.log('🧰🧰 EVENT eventMetadata', eventMetadata)
+		console.log('🧰🧰 EVENT eventMetadata bagItems', bagitems)
 
 		try {
 			await dbConnect()
 
 			try {
-				IS_USER = await User.findOne({ email: clientEmail })
+				IS_USER = await User.findOne({ email })
 			} catch (error) {
 				IS_USER = false
 			}
 
 			if (IS_USER === false) {
-				// Create a new user document
-				console.log('🧰Customer details', clientMetadata)
-
-				const userOptions = {
-					firstName: clientMetadata.firstName,
-					lastName: clientMetadata.lastName,
-					email: clientEmail,
-					phoneNumber: '1234_dummy_numberfor_now',
-					confirmPassword: AUTO_USER_PASSWORD,
-					password: AUTO_USER_PASSWORD,
-				}
-				console.log('🧰🧰 UserOptions', userOptions)
 				try {
-					const newUser = await User.create(userOptions)
-					console.log('👍 New User', newUser)
+					// Create a new user document
+					await User.create({
+						firstName: firstName,
+						lastName: lastName,
+						email,
+						phoneNumber: '1234_dummy_numberfor_now',
+						confirmPassword: process.env.ON_PAY_PAYSTACK_WEBHOOK_USER,
+						password: process.env.ON_PAY_PAYSTACK_WEBHOOK_USER,
+					})
 				} catch (error) {
-					console.log('⚠ Error creaing user', error.message)
+					console.log('⚠ Error creating user', error.message)
 				}
-			}
-
-			const fmtDateConfig = {
-				day: 'numeric',
-				month: 'long',
-				year: 'numeric',
-			}
-
-			const formattedPaidAt = new Date(EVENT.paidAt).toLocaleDateString(
-				'en-US',
-				fmtDateConfig
-			)
-
-			//Create order document
-			const orderOptions = {
-				currency: EVENT.data.currency,
-				items: EVENT.data.metadata['bag_items'],
-				paystack_ref: EVENT.data.reference,
-				payment_method: EVENT.data.channel,
-				paystack_fees: +EVENT.data.fees / 100,
-				paid_at: formattedPaidAt,
-				payment_status: EVENT.data.status,
-				totalAmount: +EVENT.data.amount / 100,
-				userEmail: clientEmail,
 			}
 
 			try {
-				const newOrder = await Order.create(orderOptions)
+				//Create order document
+				const newOrder = await Order.create({
+					currency: eventData.currency,
+					items: bagitems,
+					paystack_ref: eventData.reference,
+					payment_method: eventData.channel,
+					paystack_fees: +eventData.fees / 100,
+					paid_at: Date.now(),
+					payment_status: eventData.status,
+					totalAmount: +eventData.amount / 100,
+					userEmail: email,
+				})
 				console.log('🧰 New Order', newOrder)
 			} catch (error) {
 				console.log('🧰Error creating order document', error.message)
