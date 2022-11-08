f330caeab0b2397b273da246fc44c7be19322481
Desmond Odion <d_clipse@icloud.com>
2022-11-08 03:41:37 +0000

    Deleted wrong import api/checkout/webhook-checkout.js

--- pages/api/checkout/webhook-checkout.js
+++ pages/api/checkout/webhook-checkout.js
@@ -1,7 +1,6 @@
 import handler from '../../../controllers/app-controller'
-import handleError from '../../../controllers/error-controller'
 import { webhook_checkout } from '../../../controllers/paystack-controller'
 
-handleError.post(webhook_checkout)
+handler.post(webhook_checkout)
 
 export default handler
